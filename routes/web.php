deb3c4246ae988468cefe9674e59a02c3f8fd797
Benja <benjamacias01@gmail.com>
2026-06-01 23:02:45 -0300

    cambios get/add

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ Route::middleware(['event.active'])->group(function () {
         ->middleware('throttle:30,1')
         ->name('play.answer');
     Route::get('/play/{attempt}/result', [TriviaController::class, 'result'])->name('play.result');
-    Route::post('/play/{attempt}/next', [TriviaController::class, 'next'])
+    Route::match(['get', 'post'], '/play/{attempt}/next', [TriviaController::class, 'next'])
         ->middleware('throttle:10,1')
         ->name('play.next');
     Route::post('/play/{attempt}/close', [TriviaController::class, 'close'])
